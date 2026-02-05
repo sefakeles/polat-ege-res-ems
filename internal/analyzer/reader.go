f882a5057d759a9cd0e2734b1ddbf3ce12e176f8
Sefa Keleş <sefakeles@gmail.com>
2026-02-06 01:39:54 +0300

    commit

--- internal/analyzer/reader.go
+++ internal/analyzer/reader.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // readBaseData reads the base data from the energy analyzer
 func (s *Service) readBaseData() error {
-	data, err := s.client.ReadInputRegisters(s.ctx, BaseDataStartAddr, BaseDataLength)
+	data, err := s.client.ReadHoldingRegisters(s.ctx, BaseDataStartAddr, BaseDataLength)
 	if err != nil {
 		return fmt.Errorf("failed to read registers: %w", err)
 	}
